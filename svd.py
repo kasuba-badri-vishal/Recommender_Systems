647a60010bea04a5b5bde0f321e9ee096a73aed9
rohith203 <saranga.rohith@gmail.com>
2019-11-23 16:15:50 +0530

    some changes in svd

--- svd.py
+++ svd.py
@@ -1,5 +1,6 @@
 import time
 import numpy as np
+import pandas as pd
 
 def get_svd(M, energy=1):
 	"""
@@ -27,10 +28,8 @@ def get_svd(M, energy=1):
 	MMT = np.matmul(M, M.T)
 	# shape of MTM = n x n
 	MTM = np.matmul(M.T, M)
-	eigenvals_u, U = np.linalg.eigh(MMT)
+	_, U = np.linalg.eigh(MMT)
 	eigenvals, V = np.linalg.eigh(MTM)
-	U1 = U.copy()
-	V1 = U.copy()
 
 	for i in range(eigenvals.shape[0]):
 		eigenvals[i] = 0 if eigenvals[i]<1e-6 else eigenvals[i]
@@ -47,8 +46,20 @@ def get_svd(M, energy=1):
 	V = V[:, ::-1]
 
 	# print(U.shape, sigma.shape, V.shape)
-	print("error before reduction: ", np.linalg.norm(M-U@sigma@(V.T)))
-	
+	# print("error before reduction: ", np.linalg.norm(M-U@sigma@(V.T)))
+	if energy<1:
+		U,sigma,V = reduce_dim(U, sigma, V, eigenvals, energy)
+
+	print(U.shape,sigma.shape, V.shape)
+	print("error after reduction: ", np.linalg.norm(M-U@sigma@(V.T)))
+
+	return U,sigma,V.T
+
+def reduce_dim(U, sigma, V, eigenvals, energy):
+	'''
+	This function reduces the dimensions of 
+	the decomposition according to the given energy.
+	'''
 	i = 0
 	while i<min(sigma.shape[0],sigma.shape[1]):
 		# removing columns from U and rows from sigma
@@ -72,22 +83,47 @@ def get_svd(M, energy=1):
 	U = U[:,:num+1]
 	V = V[:,:num+1]
 	sigma = sigma[:num+1, :num+1]
-	# print(U.shape,sigma.shape, V.shape)
-	print("error after reduction: ", np.linalg.norm(M-U@sigma@(V.T)))
+	return U,sigma,V
 
-	return U,sigma,V.T
-
-def get_query_(query, df):
-	U,sigma,VT = get_svd(df)
-	return (query @ sigma)
-
-def libsvd(M):
+def libsvd(M, energy=1):
 	'''
 	SVD using numpy library function.
 	'''
 	U,d,VT = np.linalg.svd(M)
 	s = np.zeros((U.shape[1], VT.shape[0]))
 	s[:d.shape[0],:d.shape[0]] = np.diag(d)
+	if energy<1:
+		U, s, V = reduce_dim(U, s, VT.T, d, energy)
+		VT = V.T
 	print(U.shape, s.shape, VT.shape)
-	print("error: ", np.linalg.norm(M-U@s@VT))
-	return U,s,VT
+	reconstr = U@s@VT
+	print("Mean Squared error: ", np.linalg.norm(M-reconstr)/(M.shape[0]*M.shape[1]))
+	print("Mean Average error: ", np.linalg.norm(M-reconstr, 1)/(M.shape[0]*M.shape[1]))
+	return U, s, VT
+
+def get_new_ratings(userRatings, decomp):
+	'''
+	This function, when given ratings of new users
+	atleast one movie, gives ratings for other movies.
+	'''
+
+	# U, sigma, VT = libsvd(M, energy)
+	
+	V = decomp[2].T
+	res = userRatings @ V @ V.T
+	return res
+
+if __name__=='__main__':
+	starttime = time.time()
+	utility_matrix = pd.read_pickle("utility_matrix.pickle").fillna(0)
+	print("got pickle in ", time.time() - starttime)
+
+	
+	starttime = time.time()
+	decomp = libsvd(utility_matrix.values)
+	print("got svd in ", time.time() - starttime)
+
+	starttime = time.time()
+	get_new_ratings = (utility_matrix.values, decomp)
+	print("got prediction in ", time.time() - starttime)
+	
